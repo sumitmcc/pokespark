497e8c234ecdc85fd7efa24c539b61e77dd5d22e
Sumit Chachadi <schachad@cisco.com>
2018-05-04 12:56:26 -0400

    added evolution chain

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 import re
+
+import bs4 as bs4
+
 from pokespark.config import PRODUCTION
 import requests
 
@@ -11,7 +14,7 @@ class Bot:
     def __init__(self, api):
         self.api = api
         self.triggers = {
-            '^.*[Pp][Oo][Kk][Ee][Mm][Oo][Nn].*$': self.get_pokemon
+            '^.*[Pp][Oo][Kk][Ee][Mm][Oo][Nn]\s.*$': self.get_pokemon
         }
 
         self.trigger_word_appears = False
@@ -30,30 +33,53 @@ class Bot:
             message = self.api.messages.get(data['data']['id']).text
             accepted_message = True
 
-        if not PRODUCTION and self.trigger_word_appears or PRODUCTION:
+        if (not PRODUCTION and self.trigger_word_appears) or PRODUCTION:
             if accepted_message and data['data'].get('roomType') == 'direct' or data['data']['roomId'] in approved_room:
                 message = message.replace(self.api.people.me().displayName, "", 1).strip()
 
                 for regex, func in self.triggers.items():
                     if re.search(regex, message):
-                        message = message.replace('pokemon', "", 1).strip()
+                        message = message.lower().replace('pokemon', "", 1).strip()
                         func(data, message)
                         break
 
     def get_pokemon(self, data, message):
         message = requests.get(poke_url+message).json()
+        if message.get('detail') and message['detail'] == "Not found.":
+            self.api.messages.create(
+                    markdown="Missed something? This might help you {}"
+                             "".format("https://pokemondb.net/pokedex/national"),
+                    roomId=data['data']['roomId']
+            )
+            return
+
+        evolution = self.get_evolution_chain(message['forms'][0]['name'])
         reply = """...
+        Name: {name}
         Abilities: {ability},
         weight: {weight} lbs,
-        moves: {moves}
+        moves: {moves},
+        evolution chain: {evo}
         """.format(
-                ability = ','.join([x['ability']['name'] for x in message['abilities']]),
+                name = message['forms'][0]['name'],
+                ability = ', '.join([x['ability']['name'] for x in message['abilities']]),
                 weight = message['weight'],
-                moves = ",".join([x['move']['name'] for x in message['moves']])
+                moves = ", ".join([x['move']['name'] for x in message['moves']]),
+                evo = evolution
         )
 
         self.api.messages.create(
                 markdown=reply,
                 roomId=data['data']['roomId']
         )
+        self.api.messages.create(
+                text="Need more info? {website}".format(
+                        website="https://pokemondb.net/pokedex/" + message['forms'][0]['name']),
+                roomId=data['data']['roomId']
+        )
 
+    def get_evolution_chain(self, name):
+        evolution = requests.get("https://pokemondb.net/pokedex/"+str(name))
+        soup = bs4.BeautifulSoup(evolution.content, 'html.parser')
+        lass = soup.select('div span a img')
+        return ", ".join([x.get('alt') for x in lass]) if lass else "None"
